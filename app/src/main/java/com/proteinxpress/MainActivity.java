54904a1c4ecfc45672a135b0ec37fa0a157db16a
Sahil Soni <SscSPs@gmail.com>
2019-08-07 10:10:57 +0530

    Init Commit
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

---
+++ app/src/main/java/com/proteinxpress/MainActivity.java
@@ -0,0 +1,18 @@
+package com.proteinxpress;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView myWebView = new WebView(this);
+        setContentView(myWebView);
+        myWebView.loadUrl("https://proteinxpress.com");
+
+    }
+}
