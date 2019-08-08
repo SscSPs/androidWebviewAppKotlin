4e39d1b636df7c25bc0776d47073f9d7c4976b5b
Sahil Soni <SscSPs@gmail.com>
2019-08-08 11:01:24 +0530

    Add basic WebView and set some configs
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

--- app/src/main/java/com/proteinxpress/MainActivity.java
+++ app/src/main/java/com/proteinxpress/MainActivity.java
@@ -1,18 +1,49 @@
 package com.proteinxpress;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static final String websiteURL = "https://proteinxpress.com";
+    WebView myWebView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        WebView myWebView = new WebView(this);
-        setContentView(myWebView);
-        myWebView.loadUrl("https://proteinxpress.com");
 
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        myWebView = new WebView(this);
+        setContentView(myWebView);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl(websiteURL);
+        myWebView.setBackgroundColor(getResources().getColor(R.color.black));
+
+        myWebView.setWebViewClient(new MyWebViewClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
     }
 }
